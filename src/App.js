2824670ac3aa64cfda014062ae83f1f2a0474588
Leonardo Mederios <lesoname@pop-os.localdomain>
2022-12-03 04:49:07 -0300

    bootstrap styling

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ import Create from './components/create';
 function App() {
   return (
     <div className="App">
-      <div class="fs-2 p-5 position-absolute top-0 start-50 translate-middle-x">
-        React Crud Operations
+      <div class="position-absolute top-50 start-50 translate-middle">
+        <div class="fs-3">
+          React Crud Operations
+          </div>
         <div>
           <Create/>
         </div>
--- src/components/create.js
+++ src/components/create.js
@@ -1,22 +1,23 @@
 import React, { useState } from 'react'; // eslint-disable-line no-unused-vars
-import { Button, Checkbox, Form } from 'semantic-ui-react'
+import { Button, Form } from 'semantic-ui-react'
 
 
-const Create = () => (
-    <Form>
-        <Form.Field>
-            <label>First Name</label>
-            <input placeholder='First Name' />
-        </Form.Field>
-        <Form.Field>
-            <label>Last Name</label>
-            <input placeholder='Last Name' />
-        </Form.Field>
-        <Form.Field>
-            <Checkbox label='I agree to the Terms and Conditions' />
-        </Form.Field>
-        <Button type='submit'>Submit</Button>
-    </Form>
-)
-
-export default Create;
+export default function Create() {
+    const [firstName, setFirstName] = useState(''); // eslint-disable-line no-unused-vars
+    const [lastName, setLastName] = useState(''); // eslint-disable-line no-unused-vars
+    return (
+        <div>
+            <Form className="create-form">
+                <Form.Field>
+                    <label></label>
+                    <input class="form-control" placeholder='First Name' />
+                </Form.Field>
+                <Form.Field>
+                    <label ></label>
+                    <input class="form-control" placeholder='Last Name' />
+                </Form.Field>
+                <Button class="mt-4 btn btn-primary" type='submit'>Submit</Button>
+            </Form>
+        </div>
+    )
+}
