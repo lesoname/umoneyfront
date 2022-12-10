7a6f666f9a9a1d8c4139a073937dd89fc234a49d
Leonardo Mederios <lesoname@pop-os.localdomain>
2022-12-09 23:05:01 -0300

    autocomplete inputs after click on update button

--- src/components/create.js
+++ src/components/create.js
@@ -15,8 +15,7 @@ export default function Create() {
             category,
             price,
             dueDate
-    })
-}
+    })}
 
 
     return (
--- src/components/update.js
+++ src/components/update.js
@@ -1,13 +1,23 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, Form } from 'semantic-ui-react'
 import axios from 'axios';
 
 
 export default function Update() {
+    const [id, setID] = useState(null);
     const [description, setDescription] = useState('');
     const [category, setCategory] = useState('');
     const [price, setPrice] = useState('');
     const [dueDate, setDueDate] = useState('');
+    
+    useEffect(() => {
+        setID(localStorage.getItem('Id'))
+        setDescription(localStorage.getItem('Description'));
+        setCategory(localStorage.getItem('Category'));
+        setPrice(localStorage.getItem('Price'));
+        setDueDate(localStorage.getItem('Due Date'));
+    }, []);
+
     
     const postData = () => {
         axios.post('https://umoneytest.free.beeceptor.com/my/api/path', {
@@ -15,8 +25,8 @@ export default function Update() {
             category,
             price,
             dueDate
-    })
-}
+    })}
+
 
 
     return (
@@ -25,23 +35,37 @@ export default function Update() {
                 <h2 className="fs-2 mb-3"> Update a debt</h2>
             </div>
             <Form className="create-form">
+                
                 <Form.Field>
-                    <input className="form-control" placeholder='Description'
-                    onChange={(e) => setDescription(e.target.value)} />
+                    <input className="form-control"
+                        placeholder='Description'
+                        onChange={(e) => setDescription(e.target.value)}
+                        value={description}/>
                 </Form.Field>
+
                 <Form.Field>
-                    <input className="mt-3 form-control" placeholder='Category'
-                    onChange={(e) => setCategory(e.target.value)}/>
+                    <input className="mt-3 form-control"
+                    placeholder='Category'
+                    onChange={(e) => setCategory(e.target.value)}
+                    value={category}/>
                 </Form.Field>
+
                 <Form.Field>
-                    <input className="mt-3 form-control" placeholder='Price'
-                    onChange={(e) => setPrice(e.target.value)}/>
+                    <input className="mt-3 form-control"
+                    placeholder='Price'
+                    onChange={(e) => setPrice(e.target.value)}
+                    value={price}/>
                 </Form.Field>
+
                 <Form.Field>
-                    <input className="mt-3 form-control" placeholder='Due date'
-                    onChange={(e) => setDueDate(e.target.value)}/>
+                    <input className="mt-3 form-control"
+                    placeholder='Due date'
+                    onChange={(e) => setDueDate(e.target.value)}
+                    value={dueDate}/>
                 </Form.Field>
+
                 <Button type='submit' onClick={postData}className="mt-3 btn btn-primary"> Submit</Button>
+            
             </Form>
         </div>
     )
