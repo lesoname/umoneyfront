dd091e60cd94bfe45fac1f66688e36f0b0599886
Leonardo Mederios <lesoname@pop-os.localdomain>
2022-12-09 22:23:37 -0300

    update page cloned to create.js

---
+++ src/components/update.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import { Button, Form } from 'semantic-ui-react'
+import axios from 'axios';
+
+
+export default function Update() {
+    const [description, setDescription] = useState('');
+    const [category, setCategory] = useState('');
+    const [price, setPrice] = useState('');
+    const [dueDate, setDueDate] = useState('');
+    
+    const postData = () => {
+        axios.post('https://umoneytest.free.beeceptor.com/my/api/path', {
+            description,
+            category,
+            price,
+            dueDate
+    })
+}
+
+
+    return (
+        <div>
+            <div>
+                <h2 className="fs-2 mb-3"> Update a debt</h2>
+            </div>
+            <Form className="create-form">
+                <Form.Field>
+                    <input className="form-control" placeholder='Description'
+                    onChange={(e) => setDescription(e.target.value)} />
+                </Form.Field>
+                <Form.Field>
+                    <input className="mt-3 form-control" placeholder='Category'
+                    onChange={(e) => setCategory(e.target.value)}/>
+                </Form.Field>
+                <Form.Field>
+                    <input className="mt-3 form-control" placeholder='Price'
+                    onChange={(e) => setPrice(e.target.value)}/>
+                </Form.Field>
+                <Form.Field>
+                    <input className="mt-3 form-control" placeholder='Due date'
+                    onChange={(e) => setDueDate(e.target.value)}/>
+                </Form.Field>
+                <Button type='submit' onClick={postData}className="mt-3 btn btn-primary"> Submit</Button>
+            </Form>
+        </div>
+    )
+}
+
