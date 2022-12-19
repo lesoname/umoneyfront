8a808df6980389313871d881e00ac89362f9ca6f
Leonardo Mederios <lesoname@pop-os.localdomain>
2022-12-19 16:31:01 -0300

    getData function added

--- src/components/read.js
+++ src/components/read.js
@@ -27,6 +27,13 @@ export default function Read() {
         axios.delete(`http://127.0.0.1:3000/api/v1/debts/`+id)
     }
 
+    const getData = () => {
+        axios.get(`http://127.0.0.1:3000/api/v1/debts`)
+            .then((getData) => {
+                 setAPIData(getData.data);
+             })
+    }
+
 
     return (
         <div>
